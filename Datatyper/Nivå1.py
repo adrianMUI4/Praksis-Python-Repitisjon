909fcb677330be20689b245ed69ee96de5bc5117
adrian <adsaa018@osloskolen.no>
2026-01-26 20:28:53 +0100

    gjort nivå 1

---
+++ Datatyper/Nivå1.py
@@ -0,0 +1,26 @@
+#oppgave 1
+#fornavn = "Adrian"
+#alder = 17
+
+#print(fornavn)
+#print(alder)
+
+#oppgave 2
+#a = 10
+#b = 3
+
+#print(a + b)
+
+#oppgave 3
+
+poeng = 95
+print("Du fikk " + str(poeng) + " poeng")
+
+
+
+
+
+
+
+
+
