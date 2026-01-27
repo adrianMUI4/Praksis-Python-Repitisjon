098f211e94a1f36e89620f6dc421ea93d19baba7
adrian <adsaa018@osloskolen.no>
2026-01-27 12:48:02 +0100

    datatyper nivå 2 og 3

---
+++ Datatyper/nivå2.py
@@ -0,0 +1,16 @@
+
+
+#navn = input("Hva er navnet ditt? ")
+#print("Hei, " + navn)
+
+
+#tall1 = int(input("Gi meg ett tall: "))
+#tall2 = int(input("Gi meg ett annet tall: "))
+#print("Summen av disse tallene blir:", tall1 + tall2)
+
+tall1 = 2026
+tall2 = int(input("hvilket år ble du født i? "))
+print("Du er: ", tall1 - tall2)
+
+
+
---
+++ Datatyper/nivå3.py
@@ -0,0 +1,20 @@
+# filmer = ("Interstellar", "Die Hard", "Dune", "Zootropolis") 
+# print(filmer[0])
+# print(filmer[2:])
+
+# x = str(input("Hva er navnet ditt? "))
+# y = str(input("Hvor gammel er du? "))
+# z = str(input("Hva er din favoritt farge? "))
+
+# print(f"Hei, {x}\ndu er {y} år gammel\ndin favoritt farge er {z}")
+
+print("Skriv din top 3 favoritt retter")
+
+x = str(input("favoritt retten din: "))
+y = str(input("Hva er top 2: "))
+z = str(input("Hva er top 3: "))
+
+print(f"Din favoritt rett er {x}\ndu liker også {y} men ikke like mye som {x}\ndin top 3 er {z}")
+
+
+
