9ca054ce3615b9929bc56bc921c4ef0fb32ead2d
adrian <adsaa018@osloskolen.no>
2026-01-28 12:10:36 +0100

    nivå 1 av løkker

--- Datatyper/nivå3.py
+++ Datatyper/nivå3.py
@@ -8,13 +8,13 @@
 
 # print(f"Hei, {x}\ndu er {y} år gammel\ndin favoritt farge er {z}")
 
-print("Skriv din top 3 favoritt retter")
+# print("Skriv din top 3 favoritt retter")
 
-x = str(input("favoritt retten din: "))
-y = str(input("Hva er top 2: "))
-z = str(input("Hva er top 3: "))
+# x = str(input("favoritt retten din: "))
+# y = str(input("Hva er top 2: "))
+# z = str(input("Hva er top 3: "))
 
-print(f"Din favoritt rett er {x}\ndu liker også {y} men ikke like mye som {x}\ndin top 3 er {z}")
+# print(f"Din favoritt rett er {x}\ndu liker også {y}  men ikke like mye som {x}\ndin top 3 er {z}")
 
 
 
---
+++ Løkker/Nivå1.py
@@ -0,0 +1,46 @@
+# #høyde = 140
+# høyde = int(input("Hvor høy er du? "))
+
+
+# if høyde >= 150:
+#     print("Du kan ta berg-og-dal-banen ")
+# else:
+#     print("Du kan ikke ta berg-og-dal-banen ")
+
+
+
+
+# vekt = int(input("hvor mye veier bagasjen din? "))
+
+# if vekt >= 20:
+#     print("Du må betale extra") 
+# else:
+#     print("Bagasjen er OK ")
+
+
+
+
+
+
+# fart = 65
+fart = int(input("Hvor mange km i timen gikk du? "))
+
+if fart >= 80:
+    print("Høy bot")
+elif fart >= 60 and fart <= 79:
+    print("Lav bot")
+elif fart < 60:
+    print("lovlig fart")  
+
+
+
+
+    
+
+
+
+
+
+
+
+
