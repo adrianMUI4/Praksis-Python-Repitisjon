7e50c0d377e3a41a36132d03717ec902efaefbd8
adrian <adsaa018@osloskolen.no>
2026-01-28 15:25:48 +0100

    nivå2 ferdig

---
+++ Løkker/nivå2.py
@@ -0,0 +1,52 @@
+# tall1 = float(input("Skriv inn første tall: "))
+# tall2 = float(input("Skriv inn andre tall: "))
+# operasjon = input("Velg operasjon (+, -, *, /): ")
+
+# if operasjon == "+":
+#     resultat = tall1 + tall2
+#     print("Resultatet er:", resultat)
+# elif operasjon == "-":
+#     resultat = tall1 - tall2
+#     print("Resultatet er:", resultat)
+# elif operasjon == "*":
+#     resultat = tall1 * tall2
+#     print("Resultatet er:", resultat)
+# elif operasjon == "/":
+#     if tall2 == 0:
+#         print("Du kan ikke dele på 0")
+#     else:
+#         resultat = tall1 / tall2
+#         print("Resultatet er:", resultat)
+#else:
+#     print("Ugyldig operasjon")
+
+
+
+
+# passord = str(input("Hva er passordet ditt? "))
+
+# if passord > ("hemmelig123"):
+#     print("Ugyldig passord")
+# else:
+#     print("Tilgang godkjent")
+
+
+tall = int(input("gi meg et tall: "))
+
+if tall % 2 == 0:
+    print("Tallet er et partall")
+else:
+    print("tallet er et oddetall")
+
+
+
+
+
+
+
+
+
+
+
+
+
