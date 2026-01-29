c484d3c6eb151ea07722b57d98de40571a05b962
adrian <adsaa018@osloskolen.no>
2026-01-29 10:58:18 +0100

    nivå 3 ferdig

---
+++ Løkker/nivå3.py
@@ -0,0 +1,13 @@
+# for i in range(1, 11):
+#     print(i)
+
+# dyr = ["hund", "katt", "fugl", "fisk"]
+
+# for dyrene in dyr:
+#     print (f"jeg liker {dyrene} ")  
+
+
+teller = 1
+while teller < 6:
+    print(f"Teller er: {teller}")
+    teller = teller + 1 
